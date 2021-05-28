b07abecfef80dbd25db9d11d15c1d1029f2c6f6f
Michal Parusinski <parusinski@unistra.fr>
2021-05-28 14:05:59 +0200

    test: added test for dems

--- eoreader/env_vars.py
+++ eoreader/env_vars.py
@@ -17,22 +17,28 @@
 """ Environment variables that can change the processes """
 
 PP_GRAPH = "EOREADER_PP_GRAPH"
-"""Environment variables for overriding default pre-processing graph path"""
+"""Environment variable for overriding default pre-processing graph path"""
 
 DSPK_GRAPH = "EOREADER_DSPK_GRAPH"
-"""Environment variables for overriding default despeckling graph path"""
+"""Environment variable for overriding default despeckling graph path"""
 
 SAR_DEF_RES = "EOREADER_SAR_DEFAULT_RES"
-"""Environment variables for SAR default resolution, used for SNAP orthorectification to override default resolution."""
+"""Environment variable for SAR default resolution, used for SNAP orthorectification to override default resolution."""
 
 S3_DEF_RES = "EOREADER_S3_DEFAULT_RES"
-"""Environment variables for S3 default resolution, used for SNAP orthorectification to override default resolution."""
+"""Environment variable for S3 default resolution, used for SNAP orthorectification to override default resolution."""
 
 DEM_PATH = "EOREADER_DEM_PATH"
-"""Environment variables for overriding default DEM path"""
+"""Environment variable for overriding default DEM path"""
+
+S3_DB_URL_ROOT = "S3_DB_URL_ROOT"
+"""Environment variable used for specify DB base url (e.g. https://s3.unistra.fr/bucket_name/) """
+
+TEST_USING_S3_DB = "TESTING_USING_S3_DB"
+"""Environment variable to specify to use external DB as a opposed to local one. (For testing puposes only)"""
 
 CI_EOREADER_BAND_FOLDER = "CI_EOREADER_BAND_FOLDER"
 """
-Environment variables used in CI to override the existing band path
+Environment variable used in CI to override the existing band path
 in order to bypass SNAP process and DEM reprojection.
 """
