3d3dbbc853178f4b52259cd55147b61a1ae159a7
Rémi BRAUN <remi.braun@unistra.fr>
2021-07-20 16:01:23 +0200

    - CI: Do not reinstall everything if not needed (only PyYAML)

--- eoreader/__init__.py
+++ eoreader/__init__.py
@@ -17,7 +17,7 @@
 """
 **EOReader** library
 """
-__version__ = "0.4.6"
+__version__ = "0.4.7"
 __title__ = "eoreader"
 __description__ = (
     "Remote-sensing opensource python library reading optical and SAR sensors, "
