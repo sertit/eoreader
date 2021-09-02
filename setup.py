a32a770ebfc0d2dbbe127224509aa1e09868080a
Rémi BRAUN <remi.braun@unistra.fr>
2021-09-02 14:20:13 +0200

    v0.6.0

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setuptools.setup(
         "validators",
     ],
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "Natural Language :: English",
