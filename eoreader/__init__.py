610cfd49bbc6c7031dcd8d88dab7311a07a9bf3a
Rémi BRAUN <remi.braun@unistra.fr>
2021-09-10 15:59:12 +0200

    v0.6.2

--- eoreader/__init__.py
+++ eoreader/__init__.py
@@ -17,7 +17,7 @@
 """
 **EOReader** library
 """
-__version__ = "0.6.1"
+__version__ = "0.6.2"
 __title__ = "eoreader"
 __description__ = (
     "Remote-sensing opensource python library reading optical and SAR sensors, "
