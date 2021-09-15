a2d536e70c8eaf2bb6ceec5cf5165aae766039ae
Rémi BRAUN <remi.braun@unistra.fr>
2021-09-15 15:02:50 +0200

    FIX: Discard unused MIR and FNIR bands

--- eoreader/products/optical/s3_product.py
+++ eoreader/products/optical/s3_product.py
@@ -195,7 +195,7 @@ class S3Product(OpticalProduct):
                     obn.SWIR_CIRRUS: "4",  # radiance, 500m
                     obn.SWIR_1: "5",  # radiance, 500m
                     obn.SWIR_2: "6",  # radiance, 500m
-                    obn.MIR: "7",  # brilliance temperature, 1km
+                    # obn.MIR: "7",  # brilliance temperature, 1km
                     obn.TIR_1: "8",  # brilliance temperature, 1km
                     obn.TIR_2: "9",  # brilliance temperature, 1km
                 }
