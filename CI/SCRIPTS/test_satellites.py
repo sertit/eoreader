c7e31bde2dbb8248e7b8fbfead0926a88af2fc8d
Rémi BRAUN <remi.braun@unistra.fr>
2022-02-02 17:23:04 +0100

    L1D SAOCOM CI Only on S3

--- CI/SCRIPTS/test_satellites.py
+++ CI/SCRIPTS/test_satellites.py
@@ -527,11 +527,22 @@ def test_iceye():
     _test_core_sar("*SC_*")
 
 
+@pytest.mark.skipif(
+    sys.platform == "win32" or os.getenv(CI_EOREADER_S3) == "0",
+    reason="Weirdly, Landsat-5 image shape is not the same with data from disk or S3. Skipping test on disk",
+)
 @s3_env
 @dask_env
-def test_saocom():
+def test_saocom_l1d():
+    """Function testing the support of SAOCOM L1D sensor (only on S3) """
+    _test_core_sar("*L1DSARSAO*")
+
+
+@s3_env
+@dask_env
+def test_saocom_l1a():
     """Function testing the support of SAOCOM sensor"""
-    _test_core_sar("*SAO*")
+    _test_core_sar("*L1ASARSAO*")
 
 
 # TODO:
