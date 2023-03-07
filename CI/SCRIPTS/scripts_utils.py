a9cd6be77a379ce044fc91a0c6e9c15a72007085
BRAUN REMI <remi.braun@unistra.fr>
2023-03-07 13:36:39 +0100

    Convert chunk from env into int

--- CI/SCRIPTS/scripts_utils.py
+++ CI/SCRIPTS/scripts_utils.py
@@ -130,7 +130,7 @@ def dask_env(function: Callable):
                 LOGGER.info("Using DASK Local Cluster")
                 function()
         else:
-            os.environ[TILE_SIZE] = "1024"
+            os.environ[TILE_SIZE] = "4096"
             LOGGER.info("Using DASK Threading")
             function()
 
