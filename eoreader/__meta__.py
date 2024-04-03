955c1fef6fdadd5af7e66383dc760b2a3aff9097
BRAUN REMI <remi.braun@unistra.fr>
2024-04-03 16:50:03 +0200

    v0.21.1

--- eoreader/__meta__.py
+++ eoreader/__meta__.py
@@ -17,7 +17,7 @@
 """
 **EOReader** library
 """
-__version__ = "0.21.0.post0"
+__version__ = "0.21.1"
 __title__ = "eoreader"
 __description__ = (
     "Remote-sensing opensource python library reading optical and SAR constellations, "
