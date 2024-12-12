5364e73f75992040af5a197f5101a230d6d1d915
BRAUN REMI <remi.braun@unistra.fr>
2024-12-12 10:52:47 +0100

    Fix rasterize factorization for S2 products

--- CI/SCRIPTS/test_index.py
+++ CI/SCRIPTS/test_index.py
@@ -2,7 +2,6 @@
 
 import logging
 import os
-import tempfile
 
 import numpy as np
 from sertit import ci, rasters
@@ -27,7 +26,7 @@ reduce_verbosity()
 
 @s3_env
 @dask_env
-def test_index():
+def test_index(tmp_path):
     """Function testing the correct functioning of the indices"""
     # Load S2 products as it can load every index
     s2_path = opt_path().joinpath(
@@ -35,35 +34,34 @@ def test_index():
     )
     prod = READER.open(s2_path)
     failed_idx = []
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        # tmp_dir = os.path.join("/mnt", "ds2_db3", "CI", "eoreader", "DATA", "INDEX")
-        prod.output = os.path.join(tmp_dir, prod.condensed_name)
-        idx_list = [NDVI, NBR, BAI]
+    # tmp_path = "/home/data/CI/indices"
+    prod.output = os.path.join(tmp_path, prod.condensed_name)
+    idx_list = [NDVI, NBR, BAI]
 
-        LOGGER.info(f"Load selected indices (EOReader's + {idx_list})")
-        idx = prod.load(idx_list, pixel_size=RES)
+    LOGGER.info(f"Load selected indices (EOReader's + {idx_list})")
+    idx = prod.load(idx_list, pixel_size=RES)
 
-        for idx_name, idx_arr in idx.items():
-            LOGGER.info("Write and compare: %s", idx_name)
+    for idx_name, idx_arr in idx.items():
+        LOGGER.info("Write and compare: %s", idx_name)
 
-            # Write on disk
-            curr_path = os.path.join(prod.output, idx_name + ".tif")
-            ci_idx = get_ci_data_dir().joinpath(prod.condensed_name, idx_name + ".tif")
-            rasters.write(idx_arr, curr_path, dtype=np.float32)
+        # Write on disk
+        curr_path = os.path.join(prod.output, idx_name + ".tif")
+        ci_idx = get_ci_data_dir().joinpath(prod.condensed_name, idx_name + ".tif")
+        rasters.write(idx_arr, curr_path, dtype=np.float32)
 
-            # Write to path if needed
-            if not ci_idx.exists():
-                raise FileNotFoundError(f"{ci_idx} not found !")
-                # ci_idx = curr_path
+        # Write to path if needed
+        if not ci_idx.exists():
+            raise FileNotFoundError(f"{ci_idx} not found !")
+            # ci_idx = curr_path
 
-            # Test
-            try:
-                ci.assert_raster_almost_equal(curr_path, ci_idx, decimal=4)
-            except AssertionError as ex:
-                LOGGER.debug(ex)
-                failed_idx.append(idx_name)
+        # Test
+        try:
+            ci.assert_raster_almost_equal(curr_path, ci_idx, decimal=4)
+        except AssertionError as ex:
+            LOGGER.debug(ex)
+            failed_idx.append(idx_name)
 
-        # Read the results
-        # Do like that to check all existing indices
-        if failed_idx:
-            raise AssertionError(f"Failed index: {failed_idx}")
+    # Read the results
+    # Do like that to check all existing indices
+    if failed_idx:
+        raise AssertionError(f"Failed index: {failed_idx}")
