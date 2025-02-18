FIX: Fix toa radiance to toa reflectance computation for VHR data

eoreader/products/optical/dimap_v1_product.py
@@ -340,34 +340,24 @@ class DimapV1Product(VhrProduct):
         return {}
 
     def _toa_rad_to_toa_refl(
-        self, rad_arr: xr.DataArray, band: BandNames, e0: float, dt: float = None
+        self, rad_arr: xr.DataArray, e0: float, dt: float = None
     ) -> xr.DataArray:
         """
         Compute TOA reflectance from TOA radiance
 
         See
-        `here <https://www.intelligence-airbusds.com/automne/api/docs/v1.0/document/download/ZG9jdXRoZXF1ZS1kb2N1bWVudC02ODMwNQ==/ZG9jdXRoZXF1ZS1maWxlLTY4MzAy/vision-1-imagery-user-guide-20210217>`_
-        (3.2.2) for more information.
-
-        WARNING: in this formula, d**2 = 1 / sqrt(dt) !
+        `here <https://engesat.com.br/wp-content/uploads/PleiadesUserGuide-17062019.pdf>`_
+        for more information. (Appendix D page 103)
 
         Args:
             rad_arr (xr.DataArray): TOA Radiance array
-            band (BandNames): Band
             e0 (float): Solar spectral irradiance for the current band
+            dt (float): Earth-Sun distance
 
         Returns:
             xr.DataArray: TOA Reflectance array
         """
-        # Compute the coefficient converting TOA radiance in TOA reflectance
-        if not dt:
-            dt = self._sun_earth_distance_variation()
-        _, sun_zen = self.get_mean_sun_angles()
-        rad_sun_zen = np.deg2rad(sun_zen)
-        toa_refl_coeff = np.pi / (e0 * dt * np.cos(rad_sun_zen))
-
-        # LOGGER.debug(f"rad to refl coeff = {toa_refl_coeff}")
-        return rad_arr.copy(data=toa_refl_coeff * rad_arr)
+        return self._toa_rad_to_toa_refl_formula(rad_arr, e0, dt)
 
     def _get_job_id(self) -> str:
         """

eoreader/products/optical/dimap_v2_product.py
@@ -174,7 +174,7 @@ class DimapV2BandCombination(ListEnum):
     """
     DIMAP V2 products band combination
 
-    See `here <www.engesat.com.br/wp-content/uploads/PleiadesUserGuide-17062019.pdf>`_
+    See `here <https://engesat.com.br/wp-content/uploads/PleiadesUserGuide-17062019.pdf>`_
     (A.1.1.2 Variable Key Information) for more information.
     """
 
@@ -241,7 +241,7 @@ class DimapV2BandCombination(ListEnum):
 class DimapV2Product(VhrProduct):
     """
     Super Class of DIMAP V2 products.
-    See `here <www.engesat.com.br/wp-content/uploads/PleiadesUserGuide-17062019.pdf>`_
+    See `here <https://engesat.com.br/wp-content/uploads/PleiadesUserGuide-17062019.pdf>`_
     for more information.
     """
 
@@ -688,8 +688,8 @@ class DimapV2Product(VhrProduct):
         Converts band to reflectance
 
         See
-        `here <https://www.intelligence-airbusds.com/automne/api/docs/v1.0/document/download/ZG9jdXRoZXF1ZS1kb2N1bWVudC01NTY0Mw==/ZG9jdXRoZXF1ZS1maWxlLTU1NjQy/airbus-pleiades-imagery-user-guide-15042021.pdf>`_
-        (Appendix D)
+        `here <https://engesat.com.br/wp-content/uploads/PleiadesUserGuide-17062019.pdf>`_
+        (Appendix D page 103)
 
         Args:
             band_arr (xr.DataArray):
@@ -1156,8 +1156,8 @@ class DimapV2Product(VhrProduct):
         Compute DN to TOA radiance
 
         See
-        `here <https://www.intelligence-airbusds.com/automne/api/docs/v1.0/document/download/ZG9jdXRoZXF1ZS1kb2N1bWVudC01NTY0Mw==/ZG9jdXRoZXF1ZS1maWxlLTU1NjQy/airbus-pleiades-imagery-user-guide-15042021.pdf>`_
-        for more information. (Appendix D)
+        `here <https://engesat.com.br/wp-content/uploads/PleiadesUserGuide-17062019.pdf>`_
+        for more information. (Appendix D page 103)
 
         Args:
             dn_arr (xr.DataArray): DN array
@@ -1201,8 +1201,8 @@ class DimapV2Product(VhrProduct):
         Compute TOA reflectance from TOA radiance
 
         See
-        `here <https://www.intelligence-airbusds.com/automne/api/docs/v1.0/document/download/ZG9jdXRoZXF1ZS1kb2N1bWVudC01NTY0Mw==/ZG9jdXRoZXF1ZS1maWxlLTU1NjQy/airbus-pleiades-imagery-user-guide-15042021.pdf>`_
-        for more information. (Appendix D)
+        `here <https://engesat.com.br/wp-content/uploads/PleiadesUserGuide-17062019.pdf>`_
+        for more information. (Appendix D page 103)
 
         Args:
             rad_arr (xr.DataArray): TOA Radiance array
@@ -1235,16 +1235,7 @@ class DimapV2Product(VhrProduct):
                 "VALUE from Band_Solar_Irradiance not found in metadata!"
             ) from exc
 
-        # Compute the coefficient converting TOA radiance in TOA reflectance
-        dt = self._sun_earth_distance_variation()
-        _, sun_zen = self.get_mean_sun_angles()
-        rad_sun_zen = np.deg2rad(sun_zen)
-
-        # WARNING: d = 1 / sqrt(d(t))
-        toa_refl_coeff = np.pi / (e0 * dt * np.cos(rad_sun_zen))
-
-        # LOGGER.debug(f"rad to refl coeff = {toa_refl_coeff}")
-        return rad_arr.copy(data=toa_refl_coeff * rad_arr)
+        return self._toa_rad_to_toa_refl_formula(rad_arr, e0)
 
     @cache
     def get_cloud_cover(self) -> float:

eoreader/products/optical/gs2_product.py
@@ -404,7 +404,7 @@ class Gs2Product(DimapV1Product):
 
             # Compute band in radiance
             band_arr = bias + band_arr * gain
-            band_arr = self._toa_rad_to_toa_refl(band_arr, band, e_sun, sun_earth_dist)
+            band_arr = self._toa_rad_to_toa_refl(band_arr, e_sun, sun_earth_dist)
 
         # To float32
         if band_arr.dtype != np.float32:

eoreader/products/optical/maxar_product.py
@@ -1460,12 +1460,6 @@ class MaxarProduct(VhrProduct):
         """
         Compute TOA reflectance from TOA radiance
 
-        See
-        `here <https://apollomapping.com/image_downloads/Maxar_AbsRadCalDataSheet2018v0.pdf>`_
-        for more information.
-
-        WARNING: in this formula, d**2 = 1 / sqrt(dt) !
-
         Args:
             rad_arr (xr.DataArray): TOA Radiance array
             band (BandNames): Band
@@ -1473,11 +1467,7 @@ class MaxarProduct(VhrProduct):
         Returns:
             xr.DataArray: TOA Reflectance array
         """
-
         # Compute the coefficient converting TOA radiance in TOA reflectance
-        dt = self._sun_earth_distance_variation() ** 2
-        _, sun_zen = self.get_mean_sun_angles()
-        rad_sun_zen = np.deg2rad(sun_zen)
         e0 = _MAXAR_E0[self.constellation][band]
 
         if self.constellation == Constellation.WVLG:
@@ -1495,10 +1485,7 @@ class MaxarProduct(VhrProduct):
             else:
                 e0 = e0_lg
 
-        toa_refl_coeff = np.pi / (e0 * dt * np.cos(rad_sun_zen))
-
-        # LOGGER.debug(f"rad to refl coeff = {toa_refl_coeff}")
-        return rad_arr.copy(data=toa_refl_coeff * rad_arr)
+        return self._toa_rad_to_toa_refl_formula(rad_arr, e0)
 
     @cache
     def get_cloud_cover(self) -> float:

eoreader/products/optical/optical_product.py
@@ -693,9 +693,17 @@ class OpticalProduct(Product):
         )
 
     @cache
-    def _sun_earth_distance_variation(self) -> float:
+    def _sun_earth_distance(self) -> float:
         """
         Correction for the Sun-Earth distance variation
+
+        Uses ephem's function according to Maxar documentation:
+        https://resources.maxar.com/white-papers/radiometric-use-of-worldview-legion-1-and-worldview-legion-2-imagery
+
+        ___________
+
+        If not installed, uses S2 L1C algorithm described in:
+        https://sentiwiki.copernicus.eu/web/s2-processing#S2Processing-TOAReflectanceComputation
 
         It utilises the inverse square law of irradiance, under which,
         the intensity (or irradiance) of light radiating from a point source is inversely proportional to the square of the distance from the source.
@@ -704,17 +712,55 @@ class OpticalProduct(Product):
          - 0.01673 is the Earth orbit eccentricity.
          - 0.0172 is the Earth angular velocity (radians/day).
 
-        See `here <https://sentinel.esa.int/web/sentinel/technical-guides/sentinel-2-msi/level-1c/algorithm>`_ for more information.
+        This method seems to be an approximation of the one described in Maxar doc which seems to have more precision
 
         Returns:
             float: Sun-Earth distance variation
         """
-        # julian_date is the Julian Day corresponding to the acquisition date (reference day: 01/01/1950).
-        ref_julian_date = datetime(year=1950, month=1, day=1)
-        julian_date = (self.date - ref_julian_date).days + 1
+        try:
+            import ephem
+
+            sun = ephem.Sun()
+            sun.compute(self.datetime)
+            se_dist = sun.earth_distance
+        except ModuleNotFoundError:
+            # https://sentiwiki.copernicus.eu/web/s2-processing#S2Processing-TOAReflectanceComputation
+            # julian_date is the Julian Day corresponding to the acquisition date (reference day: 01/01/1950).
+            ref_julian_date = datetime(year=1950, month=1, day=1)
+            julian_date = (self.date - ref_julian_date).days + 1
+
+            # Compute Sun-Earth distance
+            se_dist = 1 - 0.01673 * np.cos(0.0172 * (julian_date - 2))
 
         # Compute Sun-Earth distance variation
-        return 1 / (1 - 0.01673 * np.cos(0.0172 * (julian_date - 2))) ** 2
+        return se_dist
+
+    def _toa_rad_to_toa_refl_formula(
+        self, rad_arr: xr.DataArray, e0: float, dt: float = None
+    ) -> xr.DataArray:
+        """
+        Compute TOA reflectance from TOA radiance
+
+        See
+        `here <https://resources.maxar.com/white-papers/radiometric-use-of-worldview-legion-1-and-worldview-legion-2-imagery>`_
+        for more information.
+
+        Args:
+            rad_arr (xr.DataArray): TOA Radiance array
+            band (BandNames): Band
+            e0 (float): Solar spectral irradiance for the current band
+
+        Returns:
+            xr.DataArray: TOA Reflectance array
+        """
+        # Compute the coefficient converting TOA radiance in TOA reflectance
+        if not dt:
+            dt = self._sun_earth_distance()
+        _, sun_zen = self.get_mean_sun_angles()
+        rad_sun_zen = np.deg2rad(sun_zen)
+        toa_refl_coeff = np.pi * dt**2 / (e0 * np.cos(rad_sun_zen))
+
+        return rad_arr.copy(data=toa_refl_coeff * rad_arr)
 
     @cache
     def get_cloud_cover(self) -> float:

eoreader/products/optical/re_product.py
@@ -320,8 +320,6 @@ class ReProduct(PlanetProduct):
         `Product specs <https://assets.planet.com/docs/Planet_Combined_Imagery_Product_Specs_letter_screen.pdf>`_
         for more information.
 
-        WARNING: in this formula, d**2 = 1 / sqrt(dt) !
-
         Args:
             rad_arr (xr.DataArray): TOA Radiance array
             band (BandNames): Band
@@ -329,15 +327,7 @@ class ReProduct(PlanetProduct):
         Returns:
             xr.DataArray: TOA Reflectance array
         """
-
-        # Compute the coefficient converting TOA radiance in TOA reflectance
-        dt = self._sun_earth_distance_variation() ** 2
-        _, sun_zen = self.get_mean_sun_angles()
-        rad_sun_zen = np.deg2rad(sun_zen)
-        eai = _RE_EAI[band]
-        toa_refl_coeff = np.pi / (eai * dt * np.cos(rad_sun_zen))
-
-        return rad_arr.copy(data=toa_refl_coeff * rad_arr)
+        return self._toa_rad_to_toa_refl_formula(rad_arr, _RE_EAI[band])
 
     def _to_reflectance(
         self,

eoreader/products/optical/sv1_product.py
@@ -702,12 +702,6 @@ class Sv1Product(VhrProduct):
         """
         Compute TOA reflectance from TOA radiance
 
-        See
-        `here <https://apollomapping.com/image_downloads/Maxar_AbsRadCalDataSheet2018v0.pdf>`_
-        for more information.
-
-        WARNING: in this formula, d**2 = 1 / sqrt(dt) !
-
         Args:
             rad_arr (xr.DataArray): TOA Radiance array
             band (BandNames): Band
@@ -727,14 +721,7 @@ class Sv1Product(VhrProduct):
             root, _ = self.read_mtd()
             e0 = float(root.findtext(".//ESUN").split(",")[band_idx])
 
-        # Compute the coefficient converting TOA radiance in TOA reflectance
-        dt = self._sun_earth_distance_variation() ** 2
-        _, sun_zen = self.get_mean_sun_angles()
-        rad_sun_zen = np.deg2rad(sun_zen)
-        toa_refl_coeff = np.pi / (e0 * dt * np.cos(rad_sun_zen))
-
-        # LOGGER.debug(f"rad to refl coeff = {toa_refl_coeff}")
-        return rad_arr.copy(data=toa_refl_coeff * rad_arr)
+        return self._toa_rad_to_toa_refl_formula(rad_arr, e0)
 
     def get_quicklook_path(self) -> str:
         """

eoreader/products/optical/vis1_product.py
@@ -345,7 +345,7 @@ class Vis1Product(DimapV1Product):
         if utils.is_uint16(band_arr):
             band_arr /= 100.0
 
-        band_arr = self._toa_rad_to_toa_refl(band_arr, band, _VIS1_E0[band])
+        band_arr = self._toa_rad_to_toa_refl(band_arr, _VIS1_E0[band])
 
         # To float32
         if band_arr.dtype != np.float32: